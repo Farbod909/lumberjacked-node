1718f687849843f5b2223f221cb9c26779d5561e
Farbod909 <rafezyfarbod@gmail.com>
2024-04-22 19:42:32 -0700

    Add necessary imports to auth module

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, UsersService],
 })
 export class AuthenticationModule {}
