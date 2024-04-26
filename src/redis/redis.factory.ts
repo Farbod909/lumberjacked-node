ae60974cf8c41eb255aff4615b50d50b2864d687
Farbod909 <rafezyfarbod@gmail.com>
2024-04-26 15:50:29 -0700

    Add connection retry strategy for redis

--- src/redis/redis.factory.ts
+++ src/redis/redis.factory.ts
@@ -4,13 +4,33 @@ import { Redis } from 'ioredis';
 export const redisClientFactory: FactoryProvider<Redis> = {
   provide: 'RedisClient',
   useFactory: () => {
+    const acceptableErrors = ['READONLY'];
+
     const redisInstance = new Redis({
       host: process.env.REDIS_HOST,
       port: +process.env.REDIS_PORT,
+      retryStrategy(times) {
+        const delay = Math.min(times * 50, 2000);
+        return delay;
+      },
+      reconnectOnError(err) {
+        for (const targetError of acceptableErrors) {
+          if (err.message.includes(targetError)) {
+            // Only reconnect when the error contains one of the acceptable error keywords.
+            return true;
+          }
+        }
+      },
     });
 
-    redisInstance.on('error', (e) => {
-      throw new Error(`Redis connection failed: ${e}`);
+    redisInstance.on('error', (err) => {
+      for (const targetError of acceptableErrors) {
+        if (err.message.includes(targetError)) {
+          return;
+        }
+      }
+
+      throw new Error(`Redis connection failed: ${err}`);
     });
 
     return redisInstance;
