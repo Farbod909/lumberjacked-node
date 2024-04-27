fba65b647601a376947d8177498152132fd77cf4
Farbod909 <rafezyfarbod@gmail.com>
2024-04-26 22:25:34 -0700

    Connect to redis on the cloud

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -9,6 +9,7 @@ export const redisClientProvider: FactoryProvider<Redis> = {
     const redisInstance = new Redis({
       host: process.env.REDIS_HOST,
       port: +process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD,
       retryStrategy(times) {
         const delay = Math.min(times * 50, 2000);
         return delay;
